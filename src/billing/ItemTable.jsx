82ea1e6f64b19ab2c613fa8ed5260a36e4454438
Shojib <rabby.semicolonit@gmail.com>
2022-06-29 16:04:23 +0600

    billing

---
+++ src/billing/ItemTable.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const ItemTable = () => {
+  return (
+    <div style={{height:"200px"}}>
+        
+        <table class="table mt-4" >
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+
+  <tbody>
+  </tbody>
+</table>
+    </div>
+  )
+}
+
+export default ItemTable
